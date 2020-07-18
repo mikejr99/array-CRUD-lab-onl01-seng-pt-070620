99d4253cc4e6f08d9e684f475f5c04ad7b8a16aa
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-18 09:45:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   randomnumbers = ["2", "5", "7", "9"]
+   radnomnumbers << "10"
 end
 
 def add_element_to_start_of_array(array, element)
